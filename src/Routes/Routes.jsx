da2f6ce6402bea677d55176d1f520185eeda8575
asm-saim <saim777m@gmail.com>
2026-07-14 22:43:26 +0600

    navbar and footer implemented

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router';
+import Root from '../pages/Root/Root';
+import Errorpage from '../pages/ErrorPage/Errorpage';
+import Home from '../pages/Home/Home';
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <div>"Hello from Routes"</div>
+        Component: Root,
+        errorElement: <Errorpage></Errorpage>,
+        children: [
+            {
+                index: true,
+                path: "/",
+                Component: Home
+            }
+        ]
     }
 ])
