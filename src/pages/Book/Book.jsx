cecf45c8c357eed7d5fbe903262634dd0b57eb05
asm-saim <saim777m@gmail.com>
2026-07-15 07:28:21 +0600

    card displayed in a grid layout

--- src/pages/Book/Book.jsx
+++ src/pages/Book/Book.jsx
@@ -7,23 +7,15 @@ const Book = ({ fetchPromise, bookData }) => {
     // console.log("d", data)
     console.log(bookData)
 
-    // author:"Paulo Coelho"
-    // bookId:8
-    // bookName:"The Alchemist"
-    // category:"Fiction"
-    // image:"https://i.ibb.co.com/zS9jsdK/18144590.jpg"
-    // publisher:"HarperCollins"
-    // rating:4.6
-    // review:"'The Great Gatsby' by F. Scott Fitzgerald is a timeless masterpiece that delves into the decadence and disillusionment of the Jazz Age. Set in the Roaring Twenties, the novel unveils the enigmatic Jay Gatsby's extravagant parties, masking a pursuit of lost love. Narrated by Nick Carraway, the story explores themes of wealth, love, and the American Dream, drawing readers into a vivid portrayal of the glittering yet elusive world of the East and West Egg. Fitzgerald's prose is both poetic and haunting, weaving a compelling narrative that transcends its era. A poignant exploration of societal excess and the human condition, 'The Great Gatsby' remains a literary gem that resonates across generations."
-    // tags:(2) ['Philosophical', 'Adventure']
-    // totalPages:177
-    // yearOfPublishing:1988
+    const { author, bookId, bookName, category, image, publisher,
+        rating, review, totalPages, yearOfPublishing } = bookData
+
 
     return (
-        <div className="card bg-base-100 w-96 shadow-sm">
-            <figure>
-                <img
-                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+        <div className="card bg-base-100 w-80 shadow-sm mx-auto bg-gray-800">
+            <figure className=' pt-5'>
+                <img className='h-60'
+                    src={image}
                     alt="Shoes" />
             </figure>
             <div className="card-body">
--- src/pages/Books/Books.jsx
+++ src/pages/Books/Books.jsx
@@ -2,7 +2,7 @@ import React, { Suspense, useEffect } from 'react';
 import { useState } from 'react';
 import Book from '../Book/Book';
 
-const Books = ({loadedData}) => {
+const Books = ({ loadedData }) => {
 
     // const [allBooks, setAllBooks] = useState([]);
     // useEffect(() => {
@@ -19,9 +19,11 @@ const Books = ({loadedData}) => {
         <div>
             <h1 className='text-4xl font-bold text-center mt-16 mb-5'>Available Books</h1>
             <Suspense fallback="Loading...">
-                {
-                    loadedData.map(bookData=><Book bookData={bookData}></Book>)
-                }
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 max-w-7xl mx-auto my-10'>
+                    {
+                        loadedData.map(bookData => <Book key={bookData.bookId} bookData={bookData}></Book>)
+                    }
+                </div>
             </Suspense>
         </div>
     );
